3b125aaf8acfdc7c3882516e25fccc5b1a4913bb
lennox44loll <151576337+lennox44loll@users.noreply.github.com>
2024-02-03 00:32:19 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Playing 00 auf GFC","Playing Skript.gg","Watching HVH"];
+const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
 
 
 let currentIndex = 0;
